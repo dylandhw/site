bb9d5ca18f7bde4b8f706a591624ee2bafab3a80
dylandhw <dylanwilkins2308@gmail.com>
2025-08-20 20:58:14 -0400

    small stuff

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { FaSatellite, FaClosedCaptioning, FaSmile } from 'react-icons/fa';
 
 interface Project {
   title: string;
